3024a1dafd86c0bd00d4b4818c893c961d4ec218
Akshitha <65284508+Akshitha-akki@users.noreply.github.com>
2022-06-29 11:40:26 +0530

    commit

---
+++ mypack/B_Protected.java
@@ -0,0 +1,11 @@
+package mypack;
+import pack.*;
+public class B_Protected extends A_Protected {
+
+	public static void main(String[] args) {
+		 B_Protected obj=new B_Protected();
+		 obj.display();
+
+	}
+
+}
---
+++ pack/A_Protected.java
@@ -0,0 +1,8 @@
+package pack;
+
+public class A_Protected {
+protected void display()
+	{
+		System.out.println("Protected");
+	}
+}
